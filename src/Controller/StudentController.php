c702b495fb6f1a2222c0c5b8d1fbf1e2cb85f114
Olga Zyk <olga.zyk.sln@gmail.com>
2018-04-19 01:25:33 +0300

    Add functionality for student validation by name

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -24,18 +24,89 @@ class StudentController extends Controller
     /**
      * @Route("/validate/{element}", name="validateStudent")
      * @Method({"POST"})
+     * @param Request $request
      * @param $element
      * @return JsonResponse
      */
-    public function validate($element)
+    public function validate(Request $request, $element)
     {
-        switch ($element) {
-            case 'Name':
-                return new JsonResponse(['valid' => true]);
-            case 'Team':
-                return new JsonResponse(['valid' => true]);
+        try {
+            $input = json_decode($request->getContent(), true)['input'];
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Invalid method', Response::HTTP_BAD_REQUEST]);
         }
 
-        return new JsonResponse(['error' => 'Invalid method'], Response::HTTP_BAD_REQUEST);
+        $students[] = $this->getStudents();
+        switch($element) {
+            case 'name':
+                return new JsonResponse(['valid' => in_array(strtolower($input), $students)]);
+                break;
+
+            case 'team':
+                return new JsonResponse(['valid' => true]);
+                break;
+        }
+        return new JsonResponse(['error' => 'Invalid method', Response::HTTP_BAD_REQUEST]);
     }
+
+    private function getJsonData()
+    {
+        return /** @lang json */
+            '{
+          "Po pamok\u0173": {
+            "mentor": "Tomas",
+            "members": [
+              "Elena",
+              "Just\u0117",
+              "Deimantas"
+            ]
+          },
+          "Tech Guide": {
+            "mentor": "Sergej",
+            "members": [
+              "Matas",
+              "Martynas"
+            ]
+          },
+          "Kelion\u0117s draugas": {
+            "mentor": "Rokas",
+            "members": [
+              "Zbignev",
+              "Aist\u0117"
+            ]
+          },
+          "Wish A Gift": {
+            "mentor": "Aistis",
+            "members": [
+              "Nerijus",
+              "Olga"
+            ]
+          },
+          "Mums pakeliui": {
+            "mentor": "Paulius",
+            "members": [
+              "Egl\u0117",
+              "Svetlana"
+            ]
+          },
+          "Motyvacin\u0117 platforma": {
+            "mentor": "Audrius",
+            "members": [
+              "Viktoras",
+              "Airidas"
+            ]
+          }
+        }';
+    }
+
+    private function getStudents() {
+        $students = [];
+        $data = json_decode($this->getJsonData(), true);
+        foreach ($data as $teamData) {
+            foreach ($teamData['members'] as $student) {
+                $students[] = strtolower($student);
+            }
+        }
+    }
+
 }
