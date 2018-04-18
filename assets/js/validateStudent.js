09a54bf69f38508021fad5b3032b4e77d29a43a1
Olga Zyk <olga.zyk.sln@gmail.com>
2018-04-19 01:24:10 +0300

    Add functionality for javascript

---
+++ assets/js/validateStudent.js
@@ -0,0 +1,42 @@
+//validation in front-end
+const axios = require('axios');
+
+let name = document.getElementById('name');
+let team = document.getElementById('team');
+let validationResultName = document.getElementById('validation-result-name');
+let validationResultTeam = document.getElementById('validation-result-team');
+
+function validateName() {
+    validationResultName.innerText = '...';
+    axios.post(validationResultName.dataset.path, {input: name.value})
+        .then(function(response) {
+            if (response.data.valid) {
+                validationResultName.innerText = ':)';
+            } else {
+                validationResultName.innerText = ':(';
+            }
+        })
+        .catch(function (error) {
+            validationResultName.innerText = error;
+        })
+}
+
+function validateTeam() {
+    validationResultTeam.innerText = '...';
+  axios.post(validationResultTeam.dataset.path, {input: team.value})
+      .then(function(response) {
+          if (response.data.valid) {
+              validationResultTeam.innerHTML = ':(';
+          } else {
+              validationResultTeam.innerText = ':(';
+          }
+      })
+      .catch(function (error) {
+          validationResultTeam.innerText = error;
+      })
+}
+
+name.onkeyup = validateName;
+name.onchange = validateName;
+team.onkeyup = validateTeam;
+team.onchange = validateTeam;
