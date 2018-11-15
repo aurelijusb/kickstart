41262f0473a044c46a52ccdfdc9ce1893b6a9037
Aurelijus Banelis <aurelijus@banelis.lt>
2018-11-15 18:13:39 +0200

    Gauti request query parametrus

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,8 +11,10 @@ class HomeController extends Controller
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $kazkas = $request->get('kazkas');
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
