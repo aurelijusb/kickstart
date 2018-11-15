1ea08911817c60e1614d71a24b99b118811019cf
Aurelijus Banelis <aurelijus@banelis.lt>
2018-11-15 18:40:31 +0200

    make:controller

---
+++ src/Controller/VakarasController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VakarasController extends AbstractController
+{
+    /**
+     * @Route("/vakaras", name="vakaras")
+     */
+    public function index()
+    {
+        return $this->render('vakaras/index.html.twig', [
+            'controller_name' => 'VakarasController',
+        ]);
+    }
+}
