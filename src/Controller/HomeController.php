79974ecc65d1468d88af7dcb15dc8d7b29a9fe6d
Matas Kulingauskas <matask@gmail.com>
2018-11-20 10:03:45 +0200

    Updated HomeController and his twig file

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,21 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Service\ProjectsData;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
+     *
+     * @param ProjectsData $projects
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(ProjectsData $projects)
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'projects' => $projects->getData(),
         ]);
     }
 }
