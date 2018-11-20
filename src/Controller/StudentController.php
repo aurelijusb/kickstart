9f1b91ae804dbc548a1aa56e75afdb6ab5b9b367
Matas Kulingauskas <matask@gmail.com>
2018-11-20 10:29:04 +0200

    Modified StudentController and its template

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -4,16 +4,29 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class StudentController extends AbstractController
 {
+    private $studentName = 'Matas Kulingauskas';
+    
     /**
      * @Route("/student", name="student")
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $projectKey = $request->get('project', '');
+        $studentName = $request->get('name', '');
+        $graduated = ($this->studentName === $studentName);
+        
         return $this->render('student/index.html.twig', [
             'controller_name' => 'StudentController',
+            'project_key' => $projectKey,
+            'student_name' => $studentName,
+            'graduated' => $graduated,
         ]);
     }
 }
