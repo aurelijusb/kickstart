d62d042e14fbff517909e643bc397b42c859f648
Matas Kulingauskas <matask@gmail.com>
2018-11-20 09:45:49 +0200

    Added service to get projects data

---
+++ src/Service/ProjectsData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Service;
+
+
+class ProjectsData
+{
+    private $dataFile = 'students.json';
+    
+    public function getData()
+    {
+        $data = file_get_contents($this->dataFile);
+        if($data === false){
+            return false;
+        }
+    
+        return \json_decode($data, true);
+    }
+    
+    /**
+     * @return string
+     */
+    public function getDataFile(): string
+    {
+        return $this->dataFile;
+    }
+    
+    /**
+     * @param string $dataFile
+     * @return ProjectsData
+     */
+    public function setDataFile(string $dataFile): ProjectsData
+    {
+        $this->dataFile = $dataFile;
+        return $this;
+    }
+    
+}
