ad8511065f0b3751968b38f535701201fd7cd8e0
Matas Kazanavicius <mataskazanavicius@Matass-MacBook-Pro.local>
2018-11-22 23:40:57 +0200

    pridėta student controller ir student route

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     */
+    public function index(Request $request)
+    {
+
+        $name = $request->get('name');
+        $project = $request->get('project');
+
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+            'name' => $name,
+            'project' => $project
+        ]);
+    }
+}
