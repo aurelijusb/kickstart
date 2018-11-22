9b0a4f6f918b28bced825f6efe127979c78a18e9
Matas Kazanavicius <mataskazanavicius@Matass-MacBook-Pro.local>
2018-11-22 14:52:20 +0200

    initial commit

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -9,14 +9,14 @@ set -e
 
 # Installing dependencies
 echo "Preparing PHP dependencies..."
-$SCRIPT_DIR/backend.sh composer install
+sh $SCRIPT_DIR/backend.sh composer install
 echo ""
 echo "Preparing JavaScript/CSS dependencies..."
 echo ""
-$SCRIPT_DIR/frontend.sh yarn
+sh $SCRIPT_DIR/frontend.sh yarn
 echo ""
 echo "Regenerating frontend files with each source update."
 echo 'Wait until you see "DONE Compiled successfully"'
 echo "Press CTRL+C to exit from terminal (error message after exiting this way – is known issue #5457)"
 echo ""
-$SCRIPT_DIR/frontend.sh yarn run encore dev --watch
+sh $SCRIPT_DIR/frontend.sh yarn run encore dev --watch
