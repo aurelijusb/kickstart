42bbba163990e53aefb5a6ee33d9255732d10f7a
Matas Kazanavicius <mataskazanavicius@Matass-MacBook-Pro.local>
2018-11-22 23:09:45 +0200

    duomenų atvaizdavimas twige

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,23 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
+
+
 {
+
+    protected $data = "Matas Kazanavicius";
+
+
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
+        $data = file_get_contents('./data.json');
+        $students = json_decode($data, true);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'data' => $students,
         ]);
     }
 }
