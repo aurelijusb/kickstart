5105d543bf746daf8fb964cc20449ffa4a2be286
Matas Kulingauskas <matask@gmail.com>
2018-11-29 21:10:17 +0200

    Pakeistas $kernel objekto gavimas. $kernerl objektas gaunamas naudojant KernelInterface

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,20 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Service\ProjectsData;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      *
+     * @param KernelInterface $kernel
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(KernelInterface $kernel)
     {
-        $webPath = $this->get('kernel')->getProjectDir() . '/public/';
-        $projects = new ProjectsData($webPath.'students.json');
+        $webPath = $kernel->getProjectDir() . '/public/';
+        $projects = new ProjectsData($webPath . 'students.json');
         
         return $this->render('home/index.html.twig', [
             'projects' => $projects->getData(),
