bfa327e3fbdae865534fe94867ae764a9a7721ff
Vaidas A <vaidastr@gmail.com>
2018-12-07 09:59:02 +0200

    Comments written in English only.

--- src/Formatter/NumberFormatter.php
+++ src/Formatter/NumberFormatter.php
@@ -28,8 +28,8 @@ class NumberFormatter
         return $result;
     }
 
-    //Milijonas ir daugiau sutrumpinami iki milijonų ir paliekami du skaičiai po kablelio.
-    //(2835779  => 2.84M; 999500 => 1.00M) (Galioja 999 500 ≤ x < ∞)
+    //Millions and more are shrinking to count of millions with to decimal digits
+    //(2835779  => 2.84M; 999500 => 1.00M) (For range 999 500 ≤ x < ∞)
     private function floatToStringMillions(float $number)
     {
         $millions =  round($number / 1000000, 2);
@@ -38,7 +38,6 @@ class NumberFormatter
     }
 
 
-    //100 000 ir daugiau trumpinami iki tūkstančių.
     //(535216  => 535K; 99950 => 100K)
     private function floatToStringHundredsThousands(float $number)
     {
@@ -46,7 +45,7 @@ class NumberFormatter
         return $thousands. "K";
     }
 
-    //1000 ir daugiau apvalinama sveikųjų tikslumu. Tarp tūkstančių ir šimtų vienas tarpas. (27533.78 => 27 534) (Galioja 1 000 ≤ x < 99 950)
+    //(27533.78 => 27 534) (For range 1 000 ≤ x < 99 950)
     //Pastaba: 999.99 => 999.99, o 999.9999 => 1 000
     private function floatToStringThousands(float $number)
     {
@@ -57,9 +56,8 @@ class NumberFormatter
     }
 
 
-    //Visi kiti skaičiai apvalinami ir po kablelio rodomi du skaitmenys. Jeigu abu jie yra nuliai - rodomas sveikas skaičius.
     //(533.1 => 533.10; 66.6666 => 66.67, 12.00 => 12)
-    //(Galioja 0 ≤ x < 1 000)
+    //(For range 0 ≤ x < 1 000)
     private function floatToStringLowerThanThousand(float $number)
     {
         $rounded = round($number, 2);
