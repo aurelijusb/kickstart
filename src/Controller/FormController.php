ec41d8567a9d9e4b447b6acc49492075adc0cc11
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-19 20:16:14 +0300

    Made Form Controller

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -2,19 +2,18 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StudentController extends AbstractController
+class FormController extends AbstractController
 {
     /**
-     * @Route("/student", name="student")
+     * @Route("/form", name="form")
      */
     public function index(Request $request)
     {
-        return $this->render('student/index.html.twig', [
+        return $this->render('form/index.html.twig', [
             'name' => $request->get('name', ''),
             'project' => $request->get('project', '')
         ]);
