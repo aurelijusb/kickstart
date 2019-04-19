87c6da4e58c84ee8639f059c59b86a424489256e
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-19 19:26:27 +0300

    Created student list & form on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,17 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $json = file_get_contents('https://nfq190418.realus.website/students.json');
+        $data = json_decode($json, true);
+//        foreach ($data as $key) {
+//            var_dump($key);
+//        }
+        var_dump($data);
+
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
+            'data' => $data,
+
         ]);
     }
 }
