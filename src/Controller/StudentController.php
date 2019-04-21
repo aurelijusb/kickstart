b86c564937267e3085fda256cf4a0044181efb4b
Darius Rusencik <darius.rusencik@gmail.com>
2019-04-22 01:27:30 +0300

    Added StudentController, student.json file

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     */
+    public function index(Request $request)
+    {
+        $name = $request->query->get('name');
+        $project = $request->query->get('project');
+
+        return $this->render('student/index.html.twig', [
+            'name' => $name,
+            'project' => $project,
+        ]);
+    }
+}
