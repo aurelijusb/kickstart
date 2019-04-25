b6d0005ad89178a6f5a0ce88973195b0dfb85caf
Ignas Morkunas <ignas.morkunas1@gmail.com>
2019-04-25 13:47:50 +0300

    Removed Home Controlled, changed Students controller path to '/'

--- src/Controller/StudentsController.php
+++ src/Controller/StudentsController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class StudentsController extends AbstractController
 {
     /**
-     * @Route("/students", name="students")
+     * @Route("/", name="students")
      */
     public function index()
     {
