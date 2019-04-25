2857847c4ae62d696f4c85cca06b43ce710f3086
Viktoras Karcikovas <45361674+Karcikovas@users.noreply.github.com>
2019-04-25 17:22:35 +0300

    Pulling Data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+
+        $bigData = file_get_contents('https://nfq190418.realus.website/students.json');
+        $projects= json_decode($bigData, true);
+
+        var_dump($projects);
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'projects' => $projects,
         ]);
     }
 }
