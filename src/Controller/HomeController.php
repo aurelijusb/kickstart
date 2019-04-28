c1d12057870d904ba1a392acc7ffaab673887692
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-28 09:33:02 +0300

    Homework Fixes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     /**
@@ -12,7 +13,8 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $json = file_get_contents('https://nfq190418.realus.website/students.json');
+        $jsonPath = __DIR__.'/../../public/students.json';
+        $json = file_get_contents($jsonPath);
         $data = json_decode($json, true);
 
         return $this->render('home/index.html.twig', [
