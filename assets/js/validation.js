1f26e99f7eeb832f018838d9c019f2a5e3622273
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-28 11:41:09 +0300

    Created validate method on Homework Controller

---
+++ assets/js/validation.js
@@ -0,0 +1,4 @@
+let nameInput = document.querySelector('#name');
+let projectInput = document.querySelector('#project');
+let nameSpan = document.querySelector('#name-result');
+let projectSpan =document.querySelector('#project-result');
