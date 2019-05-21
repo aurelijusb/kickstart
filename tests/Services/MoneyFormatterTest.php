4c1c2da4b28240cd44df740f00fdc79d7bea2bed
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-05-22 01:46:45 +0300

    Update MoneyFormatterTest to use dataProviders

--- tests/Services/MoneyFormatterTest.php
+++ tests/Services/MoneyFormatterTest.php
@@ -10,39 +10,74 @@ use ReflectionException;
 
 class MoneyFormatterTest extends TestCase
 {
+    /**
+     * @return array
+     */
+    public function providerFormat()
+    {
+        return [
+            ['2.84M', 2835779],
+            ['1.00M', 999500],
+            ['-2.84M', -2835779],
+            ['-1.00M', -999500],
+            ['283.58M', 283577901],
+            ['-283.58M', -283577901],
+            ['-124K', -123654.89],
+            ['124K', 123654.89],
+            ['-535K', -535216.9999],
+            ['535K', 535216],
+            ['100K', 99950],
+            ['-100K', -99950],
+            ['0', 0.00],
+            ['27 534', 27533.78],
+            ['1 000', 999.999999999999],
+            ['999.99', 999.99],
+            ['-27 534', -27533.78],
+            ['-1 000', -999.999999999999],
+            ['-999.99', -999.99],
+            ['533.10', 533.1],
+            ['-533.10', -533.102],
+            ['533', 533.00],
+            ['66.67',66.6666666 ]
+        ];
+    }
 
     /**
-     *
+     * @dataProvider providerFormat
+     * @param string $expectedFormat
+     * @param float $numberToFormat
      */
-    public function testFormatEur()
+    public function testFormatEur(string $expectedFormat, float $numberToFormat)
     {
         /** @var NumberFormatter|MockObject $numberFormatter */
         try {
             $numberFormatter = $this->createMock(NumberFormatter::class);
             $numberFormatter->expects($this->exactly(1))
                 ->method('format')
-                ->willReturn('2.84M');
+                ->willReturn($expectedFormat);
 
             $moneyFormatter = new MoneyFormatter($numberFormatter);
-            $this->assertEquals('2.84M €', $moneyFormatter->formatEur(2835779));
+            $this->assertEquals($expectedFormat.' €', $moneyFormatter->formatEur($numberToFormat));
         } catch (ReflectionException $e) {
         }
     }
 
     /**
-     *
+     * @dataProvider providerFormat
+     * @param string $expectedFormat
+     * @param float $numberToFormat
      */
-    public function testFormatUsd()
+    public function testFormatUsd(string $expectedFormat, float $numberToFormat)
     {
         /** @var NumberFormatter|MockObject $numberFormatter */
         try {
             $numberFormatter = $this->createMock(NumberFormatter::class);
             $numberFormatter->expects($this->exactly(1))
                 ->method('format')
-                ->willReturn('-2.84M');
+                ->willReturn($expectedFormat);
 
             $moneyFormatter = new MoneyFormatter($numberFormatter);
-            $this->assertEquals('$-2.84M', $moneyFormatter->formatUsd(-2835779));
+            $this->assertEquals('$'.$expectedFormat, $moneyFormatter->formatUsd($numberToFormat));
         } catch (ReflectionException $e) {
         }
     }
