894edbf1c80768db721f025c9183d01b2860357d
lzmog <82df7512C>
2019-05-22 07:52:21 +0300

    task-functional-tests calculation logic

--- src/Services/NumberFormatter.php
+++ src/Services/NumberFormatter.php
@@ -6,9 +6,13 @@ namespace App\Services;
 
 class NumberFormatter
 {
-    private const MILLION = 1000000;
+    private const MILLION = 999500;
     private const ONE_HUNDRED_K = 100000;
+    private const THOUSAND = 1000;
+    private const HUNDRED = 100;
     private const MILLION_SIGN = 'M';
+    private const THOUSAND_SIGN = 'K';
+
 
     /**
      * @return string
@@ -16,29 +20,28 @@ class NumberFormatter
     public function formatNumbers(): string
     {
 //        $number = (int)1555501;
-        $number = (int)1000001;
+//        $number = (int)1000001;
 //        $number = (int)999500;
 //        $number = (int)555555;
+        $number = (int)12345;
 
         switch ($number) {
             case (self::MILLION <= $number):
                 $aboveMillion = $this->aboveMillion($number);
                 break;
 
-            case (self::ONE_HUNDRED_K <= $number && self::ONE_HUNDRED_K > $number):
-                $font_size = "16";
+            case (self::ONE_HUNDRED_K <= $number && self::MILLION > $number):
+                $aboveMillion = $this->aboveOneHundredK($number);
                 break;
 
-            case ($number <= 40):
-                $font_size = "18";
+            case (self::THOUSAND <= $number && self::ONE_HUNDRED_K > $number):
+                $aboveMillion = $this->aboveThousand($number);
                 break;
 
-            case ($number <= 50):
-                $font_size = "20";
+            case (self::HUNDRED <= $number && self::THOUSAND > $number):
+                $this->underThousand($number);
                 break;
         }
-
-
 
         return $aboveMillion;
     }
@@ -50,13 +53,36 @@ class NumberFormatter
     private function aboveMillion(int $number): string
     {
         $shortNumber = number_format($number / 1000000, 2);
-        $value = self::MILLION <= $number ? $shortNumber . self::MILLION_SIGN : $number;
-
-        return strval($value);
+        return strval($shortNumber . self::MILLION_SIGN);
     }
 
-    private function aboveOneHundredK(int $number)
+    /**
+     * @param int $number
+     * @return string
+     */
+    private function aboveOneHundredK(int $number): string
     {
-        
+        $shortNumber = number_format($number / 1000);
+        return strval($shortNumber . self::THOUSAND_SIGN);
+    }
+
+    /**
+     * @param int $number
+     * @return string
+     */
+    private function aboveThousand(int $number): string
+    {
+        $shortNumber = number_format($number / 1, 0 , '', ' ');
+        return strval($shortNumber);
+    }
+
+    /**
+     * @param int $number
+     * @return string
+     */
+    private function underThousand(int $number): string
+    {
+        $shortNumber = number_format($number / 1000);
+        return strval($shortNumber . self::THOUSAND_SIGN);
     }
 }
