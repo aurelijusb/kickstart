0f32c0ca225d2ea2e1c676b1ee4d3a485240be81
Ignas Morkunas <ignas.morkunas1@gmail.com>
2019-05-23 14:48:43 +0300

    Added number formatter test

---
+++ tests/Services/NumberFormatterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Services;
+
+use App\Services\NumberFormatter;
+use PHPUnit\Framework\TestCase;
+
+class NumberFormatterTest extends TestCase
+{
+    public function numbersProvider() {
+        return [
+            //Teigiami skaičiai
+            ['2.84M', 2835779],
+            ['1.00M', 999500],
+            ['535K', 535216],
+            ['100K', 99950],
+            ['27 534', 27533.78],
+            ['999.99', 999.99],
+            ['1 000', 999.9999],
+            ['533.10', 533.1],
+            ['66.67', 66.6666],
+            ['12', 12],
+            //Neigiami skaičiai
+            ['-2.84M', -2835779],
+            ['-1.00M', -999500],
+            ['-535K', -535216],
+            ['-100K', -99950],
+            ['-27 534', -27533.78],
+            ['-999.99', -999.99],
+            ['-1 000', -999.9999],
+            ['-533.10', -533.1],
+            ['-66.67', -66.6666],
+            ['-12', -12],
+
+        ];
+    }
+
+    /**
+     * @NumberFormatterTest
+     * @dataProvider numbersProvider
+     */
+
+    public function testFormatCurrency($output, $input)
+    {
+        $numberFormatter = new NumberFormatter($output, $input);
+
+        $this->assertEquals($output, $numberFormatter->formatCurrency($input));
+
+    }
+}
