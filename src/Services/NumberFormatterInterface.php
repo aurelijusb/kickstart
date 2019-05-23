93eed0869401ccc42a09d9257bb0af8d6f59b4ad
karcikovas <viktoras.karcikovas@gmail.com>
2019-05-23 21:15:54 +0300

    Added NumberFormatterInferface2

---
+++ src/Services/NumberFormatterInterface.php
@@ -0,0 +1,10 @@
+<?php namespace App\Services;
+
+interface NumberFormatterInterface
+{
+    /**
+     * @param float $number
+     * @return string|null
+     */
+    public function formatNumber(float $number): string;
+}
---
+++ tests/Services/NumberFormatter.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Tests\Services;
+
+
+class NumberFormatter
+
+{
+
+}
