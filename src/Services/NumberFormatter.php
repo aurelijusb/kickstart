007dcd5bf973779a0edfa19537a5102c8d28688b
lukas.zmoginas <lukas.zmoginas@gmail.com>
2019-05-23 23:00:16 +0300

    task-functional-tests NumberFormatter tests completed

--- src/Services/NumberFormatter.php
+++ src/Services/NumberFormatter.php
@@ -7,82 +7,106 @@ namespace App\Services;
 class NumberFormatter
 {
     private const MILLION = 999500;
-    private const ONE_HUNDRED_K = 100000;
-    private const THOUSAND = 1000;
+    private const NEGATIVE_MILLION = -999500;
+    private const ONE_HUNDRED_K = 99950;
+    private const NEGATIVE_ONE_HUNDRED_K = -99950;
+    private const THOUSAND = 999.9999;
+    private const NEGATIVE_THOUSAND = -1000;
     private const HUNDRED = 100;
+    private const NEGATIVE_HUNDRED = -100;
+    private const ZERO = 0;
     private const MILLION_SIGN = 'M';
     private const THOUSAND_SIGN = 'K';
 
 
     /**
+     * @param float $number
      * @return string
      */
-    public function formatNumbers(): string
+    public function formatNumbers(float $number): string
     {
-//        $number = (int)1555501;
+//        $number = (int)-1555501;
 //        $number = (int)1000001;
-//        $number = (int)999500;
-//        $number = (int)555555;
-        $number = (int)12345;
+//        $number = (int)1001;
+//        $number = (int)-555.23;
+//        $number = (float)-12.56;
+//        $number = (float)12.53;
+
+        $aboveMillion = 0;
 
         switch ($number) {
-            case (self::MILLION <= $number):
+            case (self::MILLION <= $number || self::NEGATIVE_MILLION >= $number):
                 $aboveMillion = $this->aboveMillion($number);
                 break;
 
-            case (self::ONE_HUNDRED_K <= $number && self::MILLION > $number):
+            case (
+                (self::ONE_HUNDRED_K <= $number && self::MILLION > $number)
+                || (self::NEGATIVE_ONE_HUNDRED_K >= $number && self::NEGATIVE_MILLION < $number)
+            ):
                 $aboveMillion = $this->aboveOneHundredK($number);
                 break;
 
-            case (self::THOUSAND <= $number && self::ONE_HUNDRED_K > $number):
+            case (
+                (self::THOUSAND <= $number && self::ONE_HUNDRED_K > $number)
+                || (self::NEGATIVE_THOUSAND >= $number && self::NEGATIVE_ONE_HUNDRED_K < $number)
+            ):
                 $aboveMillion = $this->aboveThousand($number);
                 break;
 
-            case (self::HUNDRED <= $number && self::THOUSAND > $number):
-                $this->underThousand($number);
+            case (
+                (self::ZERO <= $number && self::THOUSAND > $number)
+                || (self::ZERO >= $number && self::NEGATIVE_THOUSAND < $number)
+            ):
+                $aboveMillion = $this->underThousand($number);
                 break;
+
         }
 
-        return $aboveMillion;
+        return strval($aboveMillion);
     }
 
     /**
-     * @param int $number
+     * @param float $number
      * @return string
      */
-    private function aboveMillion(int $number): string
+    private function aboveMillion(float $number): string
     {
         $shortNumber = number_format($number / 1000000, 2);
+
         return strval($shortNumber . self::MILLION_SIGN);
     }
 
     /**
-     * @param int $number
+     * @param float $number
      * @return string
      */
-    private function aboveOneHundredK(int $number): string
+    private function aboveOneHundredK(float $number): string
     {
         $shortNumber = number_format($number / 1000);
+
         return strval($shortNumber . self::THOUSAND_SIGN);
     }
 
     /**
-     * @param int $number
+     * @param float $number
      * @return string
      */
-    private function aboveThousand(int $number): string
+    private function aboveThousand(float $number): string
     {
         $shortNumber = number_format($number / 1, 0 , '', ' ');
+
         return strval($shortNumber);
     }
 
     /**
-     * @param int $number
+     * @param float $number
      * @return string
      */
-    private function underThousand(int $number): string
+    private function underThousand(float $number): string
     {
-        $shortNumber = number_format($number / 1000);
-        return strval($shortNumber . self::THOUSAND_SIGN);
+        $numb = number_format($number,2);
+        $shortNumber = floatval($numb);
+
+        return strval($shortNumber);
     }
 }
--- tests/Services/NumberFormatterTest.php
+++ tests/Services/NumberFormatterTest.php
@@ -12,9 +12,136 @@ class NumberFormatterTest extends TestCase
     /** @var NumberFormatter */
     private $number;
 
-    public function testNumberFormatter()
+    /**
+     * @dataProvider getMillionData
+     * @param float $number
+     * @param string $expectedNumber
+     */
+    public function testNumberFormatter(float $number, string $expectedNumber): void
     {
-        $this->assertIsFloat($this->number->formatNumbers());
+//        $this->assertIsFloat($this->number->formatNumbers($number));
+        $this->assertEquals($this->number->formatNumbers($number), $expectedNumber);
+    }
+
+    /**
+     * @return array
+     */
+    public function getMillionData(): array
+    {
+        return [
+            [
+                1000000.00,
+                '1.00M'
+            ],
+            [
+                1055890.00,
+                '1.06M'
+            ],
+            [
+                1000055.00,
+                '1.00M'
+            ],
+            [
+                5555555.00,
+                '5.56M'
+            ],
+            [
+                999500.00,
+                '1.00M'
+            ],
+            [
+                555555.00,
+                '556K'
+            ],
+            [
+                99950.00,
+                '100K'
+            ],
+            [
+                95261.555,
+                '95 262'
+            ],
+            [
+                27533.78,
+                '27 534'
+            ],
+            [
+                1000.78,
+                '1 001'
+            ],
+            [
+                999.99,
+                '999.99'
+            ],
+            [
+                525.9959,
+                '526'
+            ],
+            [
+                211.10,
+                '211.1'
+            ],
+            [
+                15.0000,
+                '15'
+            ],
+            [
+                -1000000.00,
+                '-1.00M'
+            ],
+            [
+                -1055890.00,
+                '-1.06M'
+            ],
+            [
+                -1000055.00,
+                '-1.00M'
+            ],
+            [
+                -5555555.00,
+                '-5.56M'
+            ],
+            [
+                -999500.00,
+                '-1.00M'
+            ],
+            [
+                -555555.00,
+                '-556K'
+            ],
+            [
+                -99950.00,
+                '-100K'
+            ],
+            [
+                -95261.555,
+                '-95 262'
+            ],
+            [
+                -27533.78,
+                '-27 534'
+            ],
+            [
+                -1000.78,
+                '-1 001'
+            ],
+            [
+                -999.99,
+                '-999.99'
+            ],
+            [
+                -525.9959,
+                '-526'
+            ],
+            [
+                -211.10,
+                '-211.1'
+            ],
+            [
+                -15.0000,
+                '-15'
+            ],
+        ];
     }
 
     /**
