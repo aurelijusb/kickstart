54b10863e0098f9d8c4c2a6145ac07265ab74f32
Docker fake user <docker.fake.user@example.com>
2019-05-26 22:36:06 +0000

    Updated phpunit homework

--- tests/NumberFormatterTest.php
+++ tests/NumberFormatterTest.php
@@ -7,30 +7,40 @@ use PHPUnit\Framework\TestCase;
 
 class NumberFormatterTest extends TestCase
 {
-    public function testValidNumber()
+    public function providerValidNumber(): array
+    {
+        return [
+            ['2.84M', '2835779'],
+            ['1.00M', '999500'],
+            ['535K', 535216],
+            ['100K', 99950],
+            ['27 534', 27533.78],
+            ['1 000', 999.9999],
+            ['999.99', 999.99],
+            ['533.10', 533.1],
+            ['66.67', 66.6666],
+            ['12.00',12],
+            ['-2.84M', -2835779],
+            ['-1.00M', -999500],
+            ['-535K', -535216],
+            ['-100K', -99950],
+            ['-27 534', -27533.78],
+            ['-1 000', -999.9999],
+            ['-999.99', -999.99],
+            ['-533.10', -533.1],
+            ['-66.67', -66.6666],
+            ['-12.00', -12]
+        ];
+    }
+
+    /**
+     * @dataProvider providerValidNumber
+     * @param string $expectedNumber
+     * @param int    $actualNumber
+     */
+    public function testValidNumber($expectedNumber, $actualNumber)
     {
         $numbers = new NumberFormatter();
-
-        $this->assertEquals('2.84M', $numbers->numberFormatter(2835779));
-        $this->assertEquals('1.00M', $numbers->numberFormatter(999500));
-        $this->assertEquals('535K', $numbers->numberFormatter(535216));
-        $this->assertEquals('100K', $numbers->numberFormatter(99950));
-        $this->assertEquals('27 534', $numbers->numberFormatter(27533.78));
-        $this->assertEquals('1 000', $numbers->numberFormatter(999.9999));
-        $this->assertEquals('999.99', $numbers->numberFormatter(999.99));
-        $this->assertEquals('533.10', $numbers->numberFormatter(533.1));
-        $this->assertEquals('66.67', $numbers->numberFormatter(66.6666));
-        $this->assertEquals('12.00', $numbers->numberFormatter(12));
-
-        $this->assertEquals('-2.84M', $numbers->numberFormatter(-2835779));
-        $this->assertEquals('-1.00M', $numbers->numberFormatter(-999500));
-        $this->assertEquals('-535K', $numbers->numberFormatter(-535216));
-        $this->assertEquals('-100K', $numbers->numberFormatter(-99950));
-        $this->assertEquals('-27 534', $numbers->numberFormatter(-27533.78));
-        $this->assertEquals('-1 000', $numbers->numberFormatter(-999.9999));
-        $this->assertEquals('-999.99', $numbers->numberFormatter(-999.99));
-        $this->assertEquals('-533.10', $numbers->numberFormatter(-533.1));
-        $this->assertEquals('-66.67', $numbers->numberFormatter(-66.6666));
-        $this->assertEquals('-12.00', $numbers->numberFormatter(-12));
+        $this->assertEquals($expectedNumber, $numbers->numberFormatter($actualNumber));
     }
 }
