4cc3afedf74bed9a4465168f8833771b88862927
Aurelijus Banelis <aurelijus@banelis.lt>
2019-11-04 21:45:34 +0200

    Homework: Symfony 1
    Spalvos: https://getbootstrap.com/docs/4.0/getting-started/theming/#variable-defaults
    HTML: Boostrap / HTML Post
    TWIG: https://symfony.com/doc/current/templating.html
    Interface Kernel: https://symfony.com/doc/current/reference/configuration/kernel.html
    Maker bundle: https://symfony.com/doc/current/bundles/SymfonyMakerBundle/index.html
    Controller request: https://symfony.com/doc/current/controller.html#the-request-object-as-a-controller-argument

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,10 +11,26 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(KernelInterface $kernel)
     {
+        $dataFile = file_get_contents($kernel->getProjectDir() . '/public/students.json');
+        $projects = json_decode($dataFile, true);
+        $students = $this->groupByStudents($projects);
+
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
+            'projects' => $projects,
+            'students' => $students,
         ]);
+    }
+
+    private function groupByStudents(array $projects) {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $result[$student] = ['project' => $projectName, 'mentors' => $project['mentors']];
+            }
+        }
+        return $result;
     }
 }
