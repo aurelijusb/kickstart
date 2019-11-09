79dbc4b9873086f7af1747dbab044a936db2c92d
Aurelijus Banelis <aurelijus@banelis.lt>
2019-11-09 22:26:45 +0200

    Debugging

--- scripts/e2e-artifacts.sh
+++ scripts/e2e-artifacts.sh
@@ -5,12 +5,16 @@ pushd `dirname $0` > /dev/null
 SCRIPT_DIR=`pwd`
 popd > /dev/null
 
+
+echo $(git config --get remote.origin.url)
 REPO=$(git config --get remote.origin.url | sed 's#https://github.com/##')
 COMMIT=$(git rev-parse --verify HEAD)
-
 DOCKER_TAG="docker.pkg.github.com/${REPO}/build-atifacts:cypress-${COMMIT}"
+echo "$COMMIT"
+echo "$REPO"
+echo "$DOCKER_TAG"
 
 echo "Will put artifacts to: ${DOCKER_TAG}"
 
-docker build ${SCRIPT_DIR}/../tests/e2e/cypress/ -t "$DOCKER_TAG"
-docker push "$DOCKER_TAG"
+#docker build -t "$DOCKER_TAG" ${SCRIPT_DIR}/../tests/e2e/cypress/
+#docker push "$DOCKER_TAG"
