4abb44946868b8d8e690ed5a73e47ca1ddfbacb7
Aurelijus Banelis <aurelijus@banelis.lt>
2019-11-09 21:00:09 +0200

    Extract cypress call to independent script
    We need to have same docker network to use container names for DNS lookup.
    Cypress container is heavy, so it is better to separate esential and long tasks.

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -5,8 +5,12 @@ pushd `dirname $0` > /dev/null
 SCRIPT_DIR=`pwd`
 popd > /dev/null
 
-docker-compose -f "$SCRIPT_DIR/docker-compose.yml" run \
+NETWORK=$(docker inspect nginx.symfony --format='{{range $networkName, $details := .NetworkSettings.Networks}}{{$networkName}}{{end}}')
+
+docker run \
+    --volume "${SCRIPT_DIR}/../tests/e2e:/e2e" \
+    --workdir="/e2e" \
     --entrypoint=cypress \
-    cypress.symfony \
-    run \
-    --env "BASE_URL=http://nginx.symfony:80"
+    --network="${NETWORK}" \
+    "cypress/included:3.2.0" \
+    run --env "BASE_URL=http://nginx.symfony:80"
