958c805d7a0f738d84331d755b501954688ac3aa
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-13 10:19:54 +0200

    Data fetched from json & displayed on twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param KernelInterface $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(KernelInterface $request)
     {
+        $file = file_get_contents($request->getProjectDir() . '/public/students.json');
+        $data = json_decode($file, true);
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'data' => $data,
         ]);
     }
 }
