8afc6c9c13fce3c23ff159ac8daff17ea301415f
Edvinas Pinkevičius <25502832+Edvinaz@users.noreply.github.com>
2019-11-13 20:28:25 +0200

    padaryti klaidu pataisymai

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -15,7 +15,7 @@ class StudentController extends AbstractController
     {
         $name = $request->get('name');
         $team = $request->get('project');
-        $success = (bool)random_int(0,1);
+        $success = (bool)random_int(0, 1);
 
         return $this->render('student/index.html.twig', [
             'name' => $name,
