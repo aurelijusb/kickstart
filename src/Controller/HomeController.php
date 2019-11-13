a2c135ea3e6cde6454953493ad88a9d772852e85
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-13 13:19:42 +0200

    Added student controller & paths on twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +12,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      * @param KernelInterface $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function index(KernelInterface $request)
     {
