2176e88d328b886e9fa7b65a6d01c5730e832bf3
Edvinas Pinkevičius <25502832+Edvinaz@users.noreply.github.com>
2019-11-13 14:11:33 +0200

    gaunam duomenis iš serverio, konvertuojam į array ir pateikiam spaudinimui

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
@@ -12,8 +11,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $json = file_get_contents('https://hw1.nfq2019.online/students.json');
+        $obj = json_decode($json, true);
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'students' => $obj,
         ]);
     }
 }
