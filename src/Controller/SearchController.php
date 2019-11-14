85d91850501d40e8ff79270b0630ba815b84cc30
arentas <arentas.meinorius@gmail.com>
2019-11-14 23:22:07 +0200

    search working

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function index(Request $request)
+    {
+
+        $path = substr($request->getRequestUri(), strlen($request->getPathInfo()) + 1, strlen($request->getRequestUri()));
+        $pos = strpos($path, '&');
+        $member = substr($path, 0, $pos);
+        $team = substr($path, $pos + 1, strlen($path));
+
+        $pos = strpos($member, '=');
+        $member = substr($member, $pos + 1, strlen($member));
+        $pos = strpos($team, '=');
+        $team = substr($team, $pos + 1, strlen($team));
+
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+            'team' => $team,
+            'member' => $member
+        ]);
+    }
+}
