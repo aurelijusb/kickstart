b5051b2a1f8bd969c5959e0b32c9ab0c57e79c66
Mantas Dobrodiejus <dobrodiejusmantas@gmail.com>
2019-11-14 21:31:26 +0200

    Namu darbai

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 
 let name = document.getElementById('name');
+let team = document.getElementById('team');
 let validationResult = document.getElementById('validation-result');
+let validationResultTeam = document.getElementById('validation-result-team');
 const validateName = function () {
     validationResult.innerText = '...';
     axios.post(validationResult.dataset.path, {input: name.value})
-        .then(function(response) {
+        .then(function (response) {
             if (response.data.valid) {
                 validationResult.innerHTML = ":)";
             } else {
@@ -16,6 +18,22 @@ const validateName = function () {
             validationResult.innerText = 'Error: ' + error;
         });
 };
+const validateTeam = function () {
+    validationResultTeam.innerText = '...';
+    axios.post(validationResultTeam.dataset.path, {input: team.value})
+        .then(function (response) {
+            if (response.data.valid) {
+                validationResultTeam.innerHTML = ":)";
+            } else {
+                validationResultTeam.innerHTML = ":(";
+            }
+        })
+        .catch(function (error) {
+            validationResultTeam.innerText = 'Error: ' + error;
+        });
+};
 
 name.onkeyup = validateName;
 name.onchange = validateName;
+team.onkeyup = validateTeam;
+team.onchange = validateTeam;
