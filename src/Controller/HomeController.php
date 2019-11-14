aa6846ddf9323227ed09414c19c851e83acd82f0
Edvinas Pinkevičius <25502832+Edvinaz@users.noreply.github.com>
2019-11-14 12:00:18 +0200

    padaryti klaidu pataisymai po komentaro

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,10 +13,23 @@ class HomeController extends AbstractController
     public function index()
     {
         $json = file_get_contents('https://hw1.nfq2019.online/students.json');
-        $students = json_decode($json, true);
+        $projects = json_decode($json, true);
+        $students = $this->groupByStudents($projects);
 
         return $this->render('home/index.html.twig', [
             'students' => $students,
+            'projects' => $projects,
         ]);
+    }
+
+    private function groupByStudents(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $result[] = ['student' => $student, 'project' => $projectName, 'mentors' => $project['mentors']];
+            }
+        }
+        return $result;
     }
 }
