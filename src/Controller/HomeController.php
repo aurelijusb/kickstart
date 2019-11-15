28c7b7477f810ff3436738c8350efb0ecfc94c4a
Kazimieras Babrauskas <b.kazimieras17@gmail.com>
2019-11-15 19:28:21 +0200

    1 try

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param KernelInterface $kernel
+     * @return Response
      */
-    public function index()
+    public function index(KernelInterface $kernel)
     {
+        $teams = json_decode(file_get_contents($kernel->getProjectDir().'/public/students.json'), true);
+        $students = [];
+        foreach ($teams as $teamName => $team) {
+            foreach ($team['students'] as $student)
+            {
+                $students [] = [
+                    'student' => $student,
+                    'project' => $teamName,
+                    'mentors' => $team['mentors']
+                ];
+            }
+        }
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'students' => $students,
         ]);
     }
 }
