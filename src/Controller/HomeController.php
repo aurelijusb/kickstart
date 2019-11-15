a376dcb24e31e56a49e622b81a6fec08c0f67ff6
Raimondas Ražinskis <feknaz@gmail.com>
2019-11-15 22:55:06 +0200

    finished homework

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,49 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $path = 'https://hw1.nfq2019.online/students.json';
+        $content = file_get_contents($path);
+        $nfqData = json_decode($content, true);
+
+        $studentsData = $this->groupByStudents($nfqData);
+        $projects = $this->getProjectTitle($nfqData);
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'studentsData' => $studentsData,
+            'projects' => $projects
         ]);
+    }
+
+
+    private function groupByStudents(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $result[] = ['student' => $student, 'project' => $projectName, 'mentors' => $project['mentors']];
+            }
+        }
+        return $result;
+    }
+
+    private function getProjectTitle(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+                $result[] = ['projectTitle' => $project['name'], 'project' => $projectName];
+        }
+        return $result;
     }
 }
