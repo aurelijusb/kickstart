ddf6d8220ae25d440154f1fbb02607b72598e189
Tomas Kleinauskas <kleinauskastom@gmail.com>
2019-11-16 00:35:39 +0200

    Symfony Homework

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +10,22 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $json = file_get_contents('https://hw1.nfq2019.online/students.json');
+        $projects = json_decode($json, true);
+        $students = $this->groupByStudents($projects);
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'students' => $students,
+            'projects' => $projects,
         ]);
+    }
+    private function groupByStudents(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $result[] = ['student' => $student, 'project' => $projectName, 'mentors' => $project['mentors']];
+            }
+        }
+        return $result;
     }
 }
---
+++ src/Controller/StudentController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     */
+    public function index(Request $request)
+    {
+        $name = $request->get('name','Nenurodytas');
+        $team = $request->get('project','Nenurodytas');
+        return $this->render('student/index.html.twig', [
+            'name' => $name,
+            'team' => $team,
+        ]);
+    }
+}
