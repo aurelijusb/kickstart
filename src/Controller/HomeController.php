3fa9319997a1d0e3530102fd382d145307daece5
Matas <matas.jarilinas@stud.vgtu.lt>
2019-11-15 22:05:12 +0200

    finished HW

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Kernel;
 
 class HomeController extends AbstractController
 {
@@ -12,8 +13,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $json = file_get_contents("https://hw1.nfq2019.online/students.json");
+        // $json = utf8_encode($json);
+        $data = json_decode($json, true);
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'someVariable' => $data
         ]);
     }
 }
