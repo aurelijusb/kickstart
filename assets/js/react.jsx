084f240bc96e948bd165f41b8196f4324dd9dd39
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-16 13:36:33 +0200

    Added controller & react on a view

--- assets/js/react.jsx
+++ assets/js/react.jsx
@@ -1,1 +1,7 @@
-let test = "testing react";
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
