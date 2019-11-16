e33abc6b3c53c41fd5ae329252b757466f7dca0d
jonagade <jonas.gadeikis9@gmail.com>
2019-11-16 18:17:29 +0200

    Homework commit

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return $this->render('student/index.html.twig', [
+            'student' => $request->get('name'),
+            'project' => $request->get('project'),
+        ]);
+    }
+}
