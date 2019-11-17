92ba3260808bc418705e1b76f6f3be2c0aefcb90
arnas277 <Arnas277@gmail.com>
2019-11-17 22:58:37 +0000

    Perdarytas namu darbas, pasidarius fork githube, kad butu galima daryti pull request

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request)
+    {
+        return $this->render('student/index.html.twig', [
+           'name' => $request->get('name'),
+           'project' => $request->get('project')
+        ]);
+    }
+}
