07623f8d06c6d9494d2e7ef3c1be73f6d79b5b6e
Diana V <d.valaityte.97@gmail.com>
2019-11-17 11:11:21 +0200

    List of students

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -16,6 +16,7 @@ class StudentController extends AbstractController
         $a = $request->getProjectDir();
         return $this->render('student/index.html.twig', [
             'controller_name' => $a,
+            'team_list' => \json_decode(\file_get_contents($a . '/assets/js/students.json'), true)
         ]);
     }
 }
