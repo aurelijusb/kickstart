67bc07a519f81ede99e15fa04286c4ed5413a1e9
Diana V <d.valaityte.97@gmail.com>
2019-11-17 19:52:05 +0200

    Done.

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -11,44 +11,53 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PeopleController extends AbstractController
 {
-    /**
-     * @Route("/people", name="people")
-     */
-    public function index()
-    {
-        return $this->render('people/index.html.twig', [
-            'controller_name' => 'PeopleController',
-        ]);
+  /**
+   * @Route("/people", name="people")
+   */
+  public function index()
+  {
+    return $this->render('people/index.html.twig', [
+      'controller_name' => 'PeopleController',
+    ]);
+  }
+
+  /**
+   * @Route(
+   *     "/validate/{element}",
+   *     name="validatePerson",
+   *     methods={"POST"}
+   * )
+   */
+  public function validate(Request $request, string $element)
+  {
+    try {
+      $input = json_decode($request->getContent(), true)['input'];
+    } catch (Exception $e) {
+      return new JsonResponse(['error' => 'Invalid method'], Response::HTTP_BAD_REQUEST);
     }
 
-    /**
-     * @Route(
-     *     "/validate/{element}",
-     *     name="validatePerson",
-     *     methods={"POST"}
-     * )
-     */
-    public function validate(Request $request, string $element)
-    {
-        try {
-            $input = json_decode($request->getContent(), true)['input'];
-        } catch (Exception $e) {
-            return new JsonResponse(['error' => 'Invalid method'], Response::HTTP_BAD_REQUEST);
+    $students = $this->getStudents();
+    switch ($element) {
+      case 'name':
+        return new JsonResponse(['valid' => in_array(strtolower($input), $students)]);
+      case 'team':
+        $studentTeam = $this->getStudentTeam($input);
+        $input2 = json_decode($request->getContent(), true)['input2'];
+        $isValidTeam = false;
+        if ($input) {
+          $isValidTeam = (strtolower($input2) === $studentTeam);
         }
-
-        $students = $this->getStudents();
-        switch ($element) {
-            case 'name':
-                return new JsonResponse(['valid' => in_array(strtolower($input), $students)]);
-        }
-
-        return new JsonResponse(['error' => 'Invalid arguments'], Response::HTTP_BAD_REQUEST);
+        return new JsonResponse(['valid' => $isValidTeam]);
     }
 
-    private function getStorage()
-    {
-        return /** @lang json */
-        '{
+    return new JsonResponse(['error' => 'Invalid arguments'], Response::HTTP_BAD_REQUEST);
+  }
+
+  private function getStorage()
+  {
+    return
+      /** @lang json */
+      '{
           "team1": {
             "name": "Team1",
             "mentors": [
@@ -229,17 +238,30 @@ class PeopleController extends AbstractController
             ]
           }
         }';
-    }
+  }
 
-    private function getStudents(): array
-    {
-        $students = [];
-        $storage = json_decode($this->getStorage(), true);
-        foreach ($storage as $teamData) {
-            foreach ($teamData['students'] as $student) {
-                $students[] = strtolower($student);
-            }
-        }
-        return $students;
+  private function getStudents(): array
+  {
+    $students = [];
+    $storage = json_decode($this->getStorage(), true);
+    foreach ($storage as $teamData) {
+      foreach ($teamData['students'] as $student) {
+        $students[] = strtolower($student);
+      }
     }
+    return $students;
+  }
+
+  private function getStudentTeam(string $student_name)
+  {
+    $storage = json_decode($this->getStorage(), true);
+    foreach ($storage as $teamData) {
+      foreach ($teamData['students'] as $student) {
+        if (strtolower($student_name) == strtolower($student)) {
+          return strtolower($teamData['name']);
+        }
+      }
+    }
+    return null;
+  }
 }
