430b627fa79678d63a91ddf39725d3a5d37630c9
MyRuonis <kciulcinskas@gmail.com>
2019-11-18 19:32:02 +0200

    Kristijonas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,12 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $data = file_get_contents("https://hw1.nfq2019.online/students.json");
+        $data = json_decode($data, true);
+
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
+            'data' => $data,
         ]);
     }
 }
