338302f281a0fa7026231299389f2835367a93aa
Lukas Patackas <xxlas@users.noreply.github.com>
2019-11-18 23:30:34 +0200

    Added homework code.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,10 +12,23 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+        /*foreach(json_decode($this->getTeams()) as $stud){
+            print_r($stud);
+        }*/
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'controller_name' => 'StudentController',
+            'teams' => json_decode($this->getTeams(), true),
         ]);
+    }
+
+    public function getTeams()
+    {
+        $client = new Client();
+        $response = $client->request('GET', 'https://hw1.nfq2019.online/students.json');
+        $response->getStatusCode();
+        $response->getHeaderLine('application/json; charset=utf8');
+        return $response->getBody()->getContents();
     }
 }
