68a4711ef41285ec3b56df9334acea02427a6f7f
Ricardas <rihaddas@gmail.com>
2019-11-19 21:56:09 +0200

    Fixed Formatting, now html is sent correctly

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request)
+    {
+        $name = $request->get('name');
+        $project=$request->get('project');
+        $myName = false;
+
+        if($name == 'Ričardas') $myName=true;
+
+        return $this->render('student/index.html.twig', [
+            'name' => $name,
+            'project' => $project,
+            'myName' => $myName
+        ]);
+    }
+}
