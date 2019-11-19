7c8b0931691f472d3ad52b443567188a6676eb4a
Justina Zidziunaite <j.zidziunaite@gmail.com>
2019-11-20 00:03:41 +0200

    Homework tasks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(KernelInterface $request)
     {
+        $data = file_get_contents($request->getProjectDir() . '/public/students.json');
+        $projects = json_decode($data, true);
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'projects' => $projects
         ]);
     }
 }
