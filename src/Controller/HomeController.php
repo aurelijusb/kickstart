abc7ac9aceb84baa771020058b612c1d9f8f1af1
Titas Dyglys <titasdg@gmail.com>
2019-11-19 22:59:11 +0200

    commitas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class  HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
+        $jsonContent = file_get_contents("https://hw1.nfq2019.online/students.json");
+        $encode = json_decode($jsonContent, true);
+        $array = (array) $encode;
+        $sutends = $this->groupByStudents($encode);
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
+            'json' => $sutends,
         ]);
     }
+    private function groupByStudents(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $result[] = ['student' => $student, 'project' => $projectName, 'mentors' => $project['mentors']];
+            }
+        }
+        return $result;
+    }
+
 }
