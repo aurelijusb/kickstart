2bd4e82b65bfe5a0867815e575536b9b6b5712b3
Kristina Rudzinskaitė <rudzinskaitekristina>
2019-11-19 22:27:22 +0200

    Create list of students and projects, create single student page with grade

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,29 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $projects = json_decode(file_get_contents('https://hw1.nfq2019.online/students.json'), true);
+        $students = $this->groupByStudents($projects);
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'students' => $students,
+            'projects' => $projects,
+            'title' => 'Projektai'
         ]);
+    }
+
+    private function groupByStudents(array $projects)
+    {
+        $students = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $students[] = [
+                    'name' => $student,
+                    'project' => $projectName,
+                    'mentors' => $project['mentors']
+                ];
+            }
+        }
+
+        return $students;
     }
 }
