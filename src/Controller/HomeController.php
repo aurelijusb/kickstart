ad36b15de5c0236495d3c9eacde94daebc0f66d1
django11 <andrius@vaskys.net>
2019-11-19 23:38:12 +0200

    home work symfony 1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Services\StudentDataService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index()
     {
+        $data = StudentDataService::getData();
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'teams' => $data['teams'],
+            'students' => $data['students'],
         ]);
     }
 }
