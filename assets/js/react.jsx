072d0dc67d05072151279d92914ae6b53d097c22
Edvinas Pinkevičius <25502832+Edvinaz@users.noreply.github.com>
2019-11-19 10:54:43 +0200

    ...add navbar

--- assets/js/react.jsx
+++ assets/js/react.jsx
@@ -1,4 +1,9 @@
-ReactDOM.render(
-    <h1>Hello world!!</h1>
-    document.getElementById('root')
-);
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+if (typeof usingReactApp !== "undefined") {
+    ReactDOM.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('root')
+    );
+}
