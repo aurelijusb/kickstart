593f07af3a6b42e3e7a50a400fabec4b1679e5a9
am <miklovisarvydas@gmail.com>
2019-11-21 02:44:19 +0200

    some memes

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -10,9 +10,9 @@ const validateName = function () {
     axios.post(validationResultName.dataset.path, {input: name.value})
         .then(function (response) {
             if (response.data.valid) {
-                validationResultName.innerHTML = ":)";
+                validationResultName.innerHTML = "<img src=https://reactiongifs.me/wp-content/uploads/2019/02/Macho-Man-Smile.gif width=\"200px\" height=\"150px\" alt=\"alt\">";
             } else {
-                validationResultName.innerHTML = ":(";
+                validationResultName.innerHTML = "<img src=https://media1.giphy.com/media/nstGUYA6WnWJa/giphy.gif?cid=790b7611e958e0e51adbfce26cccda017a20761d1ac59c6d&rid=giphy.gif width=\"200px\" height=\"150px\" alt=\"alt\">";
             }
         })
         .catch(function (error) {
@@ -25,9 +25,9 @@ const validateTeam = function () {
     axios.post(validationResultTeam.dataset.path, {input: team.value})
         .then(function (response) {
             if (response.data.valid) {
-                validationResultTeam.innerHTML = ":)";
+                validationResultTeam.innerHTML = "<img src=https://media.giphy.com/media/cJMmZY7jSCtwN4z0WX/giphy.gif width=\"200px\" height=\"150px\" alt=\"alt\">";
             } else {
-                validationResultTeam.innerHTML = ":(";
+                validationResultTeam.innerHTML = "<img src=https://media.giphy.com/media/3o6Zt7j5rtQUNPiw8g/giphy.gif width=\"200px\" height=\"150px\" alt=\"alt\">";
             }
         })
         .catch(function (error) {
