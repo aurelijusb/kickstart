d562a71ca472e8e0d4831b898805be845bd53ec7
Kristina Rudzinskaitė <rudzinskaitekristina>
2019-11-21 21:16:06 +0200

    Add validation by project title

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,21 +1,26 @@
 const axios = require('axios');
 
 let name = document.getElementById('name');
-let validationResult = document.getElementById('validation-result');
-const validateName = function () {
-    validationResult.innerText = '...';
-    axios.post(validationResult.dataset.path, {input: name.value})
+let team = document.getElementById('team');
+let validationResultName = document.getElementById('validation-result-name');
+let validationResultTeam = document.getElementById('validation-result-team');
+
+const validate = (elementResult, inputResult) => () => {
+    elementResult.innerText = '...';
+    axios.post(elementResult.dataset.path, {input: inputResult.value})
         .then(function(response) {
             if (response.data.valid) {
-                validationResult.innerHTML = ":)";
+                elementResult.innerHTML = ":)";
             } else {
-                validationResult.innerHTML = ":(";
+                elementResult.innerHTML = ":(";
             }
         })
         .catch(function (error) {
-            validationResult.innerText = 'Error: ' + error;
+            elementResult.innerText = 'Error: ' + error;
         });
 };
 
-name.onkeyup = validateName;
-name.onchange = validateName;
+name.onkeyup = validate(validationResultName, name);
+name.onchange = validate(validationResultName, name);
+team.onkeyup = validate(validationResultTeam, team);
+team.onchange = validate(validationResultTeam, team);
