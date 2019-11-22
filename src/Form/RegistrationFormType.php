5775dfc97f2b431a1f6ac4ab8584dbcb75b33c57
Tortonas <plankton546@gmail.com>
2019-11-22 19:44:03 +0200

    hw done

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -19,6 +19,7 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('linkedin')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
