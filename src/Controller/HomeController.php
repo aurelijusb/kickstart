6c319d73826372bf16f96b4887409ac5cd3d40cc
algyz200 <algirdas.toleikis@gmail.com>
2019-11-24 14:50:51 +0000

    Show/Hide Administravimas - hardcoded

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija',
+            'role' => 'admin',
         ]);
     }
 }
