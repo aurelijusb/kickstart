2489da07813d3ba3b3639860ee6f7ba15fae1422
Andrius Stonys <29209107+AndriusSton@users.noreply.github.com>
2019-11-25 20:58:32 +0200

    styling fix

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,8 +22,7 @@ class RegistrationController extends AbstractController
         GuardAuthenticatorHandler $guardHandler,
         Request $request,
         UserPasswordEncoderInterface $passwordEncoder
-    ): Response
-    {
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
