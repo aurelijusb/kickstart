a9ff00031afc1299028d32be852ff36fd6125522
AinisSitkauskas <ainis795@gmail.com>
2019-11-25 22:32:42 +0200

    Symfony 3 homework

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -50,6 +50,12 @@ class User implements UserInterface
      */
     private $homepage = "";
 
+    /**
+     * @var null|string Link to linkedin page
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $linkedin = "";
+
     public function getId(): ?int
     {
         return $this->id;
@@ -127,7 +133,7 @@ class User implements UserInterface
      */
     public function getPassword(): string
     {
-        return (string) $this->password;
+        return (string)$this->password;
     }
 
     public function setPassword(string $password): self
@@ -187,4 +193,24 @@ class User implements UserInterface
 
         return $this;
     }
+
+    /**
+     * @return string|null
+     */
+    public function getLinkedin(): ?string
+    {
+        return $this->linkedin;
+    }
+
+    /**
+     * @param string|null $linkedin
+     */
+    public function setLinkedin(?string $linkedin): self
+    {
+        $this->linkedin = $linkedin;
+
+        return $this;
+    }
+
+
 }
