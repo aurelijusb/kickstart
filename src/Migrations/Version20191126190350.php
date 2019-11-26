a6d58703a2167cef84bbe425761cbcd09ba54173
Andrius Simanaitis <eragonas5@gmail.com>
2019-11-26 21:21:36 +0200

    wtf is wrong with this auto migration tool :|

--- src/Migrations/Version20191126190350.php
+++ src/Migrations/Version20191126190350.php
@@ -22,7 +22,7 @@ final class Version20191126190350 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL, CHANGE password_changed password_changed DATETIME DEFAULT NULL, CHANGE homepage homepage VARCHAR(255) DEFAULT NULL, CHANGE linkedin linkedin VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD linkedin VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20191126190350 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin, CHANGE password_changed password_changed DATETIME DEFAULT \'NULL\', CHANGE homepage homepage VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE linkedin linkedin VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user DROP linkedin');
     }
 }
