715a386238ae1d24781a68d308469f15280fbfd9
Ricardas <rihaddas@gmail.com>
2019-12-08 00:29:56 +0200

    Namu dabu taisymas

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
@@ -24,5 +25,16 @@ class StudentController extends AbstractController
             'name' => $name,
             'project' => $project,
         ]);
+    }
+
+    /**
+     * @Route("/student/json", name="students-json")
+     * @param KernelInterface $kernel
+     * @return Response
+     */
+    public function studentsjson(KernelInterface $kernel)
+    {
+        $students = json_decode(file_get_contents($kernel->getProjectDir().'/public/students.json'), true);
+        return $this->json($students);
     }
 }
