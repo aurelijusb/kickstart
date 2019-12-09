8e400e46b9364ee3cddc914c5d4690c46f4d65a7
arnas277 <Arnas277@gmail.com>
2019-12-09 23:06:39 +0000

    Pataisytos namu darbu klaidos

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -16,8 +16,8 @@ class StudentController extends AbstractController
     public function index(Request $request)
     {
         return $this->render('student/index.html.twig', [
-           'name' => $request->get('name'),
-           'project' => $request->get('project')
+           'name' => $request->get('name', 'empty'),
+           'project' => $request->get('project', 'empty')
         ]);
     }
 }
