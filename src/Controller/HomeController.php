3b8e0128f5df9c76043b9d04bd1dc7e123eb667c
Titas Dyglys <titasdg@gmail.com>
2019-12-09 10:12:54 +0200

    hw fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $jsonContent = file_get_contents("https://hw1.nfq2019.online/students.json");
+        $jsonContent = file_get_contents("json/students.json");
         $encode = json_decode($jsonContent, true);
         $students = $this->groupByStudents($encode);
         return $this->render('home/index.html.twig', [
