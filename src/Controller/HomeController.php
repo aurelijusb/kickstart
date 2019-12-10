4018aa3a0ab72865f40c8b24ebb9d03260b4b654
Edvinas Pinkevičius <25502832+Edvinaz@users.noreply.github.com>
2019-12-10 10:43:56 +0200

    fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,26 +3,50 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request): Response
     {
         $json = file_get_contents('./../public/students.json');
         $projects = json_decode($json, true);
         $students = $this->groupByStudents($projects);
 
-        return $this->render('home/index.html.twig', [
-            'students' => $students,
-            'projects' => $projects,
+        $name = $request->get('name');
+        $project = $request->get('project');
+
+        if(isset($name) || isset($project)) {
+            return $this->Student($request);
+        } else {
+            return $this->render('home/index.html.twig', [
+                'students' => $students,
+                'projects' => $projects,
+            ]);
+        }
+    }
+
+    public function Student(Request $request): Response
+    {
+        $name = $request->get('name', 'nenurodyta');
+        $team = $request->get('project', 'nenurodyta');
+        $success = (bool)random_int(0, 1);
+
+        return $this->render('student/index.html.twig', [
+            'name' => $name,
+            'team' => $team,
+            'success' => $success,
         ]);
     }
 
-    private function groupByStudents(array $projects)
+    private function groupByStudents(array $projects): array 
     {
         $result = [];
         foreach ($projects as $projectName => $project) {
